da63a6b0bf2682372d2745e157d26d4d864daaf5
dhananjaypai08 <dhananjay2002pai@gmail.com>
2024-01-28 01:58:48 +0530

    lucas scanner

--- lucas/main.py
+++ lucas/main.py
@@ -1,12 +1,13 @@
 # Web3 imports
-from web3 import Web3, AsyncHTTPProvider
+from web3 import Web3, AsyncWeb3
 import json
 # Fastapi imports
 from fastapi import FastAPI, Request, Response
-from fastapi.responses import StreamingResponse
+from fastapi.responses import FileResponse
 from config import Node, Contract
 import uvicorn
 from middleware.TimeMiddleware import TimeMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 # other imports
 from loguru import logger
 from io import BytesIO
@@ -20,6 +21,21 @@ app = FastAPI()
 contract = Contract()
 node = Node()
 app.add_middleware(TimeMiddleware)
+Lighthouse_GATEWAY_URL = "https://gateway.lighthouse.storage/ipfs/"
+
+origins = [
+    "http://localhost",
+    "http://localhost:8082",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 async def read_json():
     file = open(contract.abi_path)
@@ -30,46 +46,57 @@ async def read_json():
 @app.on_event("startup")
 async def startup_event():
     global w3
-    w3 = Web3(Web3.HTTPProvider(node.url))
+    w3 = AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(node.url))
     contract_abi = await read_json()
     global contractwithsigner
     contractwithsigner = w3.eth.contract(address=contract.address, abi=contract_abi)
     logger.info(f"Connected with smart contract with address {contract.address}")
     
-@app.get("/{id}")
+@app.get("/")
 async def home(id: str):
-    val = contractwithsigner.functions.getTotalMints().call()
+    val = await contractwithsigner.functions.getTotalMints().call()
     logger.info(f"Total DeCAT Volume: {val}")
-    tokenIds = contractwithsigner.functions.getTokenIdAccount(id).call()
+    tokenIds = await contractwithsigner.functions.getTokenIdAccount(id).call()
     ans = []
     for tokenId in tokenIds:
-        tokenURI = contractwithsigner.functions.tokenURI(tokenId).call()
-        ans.append({tokenId: tokenURI})
-    logger.success(f"Fetched data for wallet address: {id}")
+        tokenURI = await contractwithsigner.functions.tokenURI(tokenId).call()
+        ans.append({"tokenId": tokenId, "tokenURI": Lighthouse_GATEWAY_URL+tokenURI})
+    logger.success(f"Fetched SBT data for wallet address: {id}")
+    return ans
+
+@app.get("/endorsements_received")
+async def home(id: str):
+    ids = await contractwithsigner.functions.getTokenIdAccountEndorsing(id).call()
+    logger.info("Data Retrieved")
+    ans = []
+    for tokenId in ids:
+        tokenURI = await contractwithsigner.functions.tokenURI(tokenId).call()
+        ans.append({"tokenId": tokenId, "tokenURI": Lighthouse_GATEWAY_URL+tokenURI})
+    logger.success(f"Fetched endorsing data for wallet address: {id}")
     return ans
 
 @app.post("/generate_qrcode")
 async def generate(request: Request):
-    print(request)
-    data = request.body
-    print(data, type(data))
+    data = await request.json()
+    str_data = json.dumps(data)
+    print(type(str_data))
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
-    qr.add_data(data)
+    qr.add_data(str_data)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
     img.save("qrcode.png")
-    return Response("Image saved locally")
+    return FileResponse("qrcode.png", media_type="application/octet-stream", filename="qrcode.png")
 
 @app.post("/scanQR")
 async def scanQR():
     cap = cv2.VideoCapture(0)
     found_qr_data = False
-    res_data = None
+    verified_data = False
     while True:
         ret, frame = cap.read()
 
@@ -81,19 +108,41 @@ async def scanQR():
 
         for obj in decoded_objects:
             data = obj.data.decode("utf-8")
-            print(type(data))
-            print(f"QR Code Data: {data}")
-            res_data = data
+            dict_data = json.loads(data)
+            print(dict_data, type(dict_data))
             found_qr_data = True
+            typeofSBT = dict_data["name"]
+            address = dict_data["walletAddress"]
+            URI = dict_data["tokenURI"]
+            logger.info(f"Fetched type: {typeofSBT}, address: {address}, URI: {URI}")
+            if typeofSBT == "DeCAT":
+                tokenIds = await contractwithsigner.functions.getTokenIdAccount(address).call()
+                for id in tokenIds:
+                    uri = await contractwithsigner.functions.tokenURI(id).call()
+                    if uri == URI: 
+                        verified_data = True
+            elif typeofSBT == "E-DeCAT":
+                tokenIds = await contractwithsigner.functions.getTokenIdAccountEndorsing(address).call()
+                for id in tokenIds:
+                    uri = await contractwithsigner.functions.tokenURI(id).call()
+                    if uri == URI:
+                        verified_data = True
 
         # Break the loop if 'q' key is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        if (cv2.waitKey(1) & 0xFF == ord('q')):
+            # Release the camera and close the window
+            cap.release()
+            cv2.destroyAllWindows()
+            return Response("Exited with no response")
         if cv2.waitKey(1) & found_qr_data:
             # Release the camera and close the window
             cap.release()
             cv2.destroyAllWindows()
-            return Response(res_data)
+            if verified_data:
+                res_string = f"The NFT with tokenURI: {URI} of wallet Address: {address} is Verified"
+            else:
+                res_string = f"Unfortunately, The NFT with tokenURI: {URI} of wallet Address: {address} is NOT Verified"
+            return Response(res_string)
 
     # Release the camera and close the window
     cap.release()
