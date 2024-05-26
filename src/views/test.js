247ed02fe39358fe4fabc31e71f86cdb7fb850ab
Janhavi Khanvilkar <janhavii@Janhavis-MacBook-Pro.local>
2024-05-26 13:47:25 +0530

    bloom

--- src/views/test.js
+++
@@ -1,9 +0,0 @@
-const { PrimeSdk } = require('@etherspot/prime-sdk');
-const primeSdk = new PrimeSdk(
-    { privateKey: '0x0101010101010101010101010101010101010101010101010101010101010101' }, 
-    { 
-      chainId: 137, 
-      projectKey: '' 
-    },
-);
-console.log(primeSdk.state);
